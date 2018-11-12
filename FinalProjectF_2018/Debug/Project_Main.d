723e9e1fd2796dbf3d2f51231acb74108d93475b
Mathew Yerian-French <Mathew Yerian-French@35.40.54.95>
2018-11-12 12:04:07 -0500

    Added Headers

---
+++ FinalProjectF_2018/Debug/Project_Main.d
@@ -0,0 +1,39 @@
+# FIXED
+
+Project_Main.obj: ../Project_Main.c
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp432p401r.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp_compatibility.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp432p401r_classic.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h
+Project_Main.obj: C:/ti/ccs810/ccsv8/ccs_base/arm/include/system_msp432p401r.h
+Project_Main.obj: ../LCD.h
+Project_Main.obj: ../serial.h
+
+../Project_Main.c: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp.h: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp432p401r.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/stdint.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_types.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_types.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp_compatibility.h: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/msp432p401r_classic.h: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/CMSIS/core_cm4.h: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/CMSIS/cmsis_compiler.h: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/CMSIS/cmsis_ccs.h: 
+C:/ti/ccs810/ccsv8/ccs_base/arm/include/system_msp432p401r.h: 
+../LCD.h: 
+../serial.h: 
--- FinalProjectF_2018/Project_Main.c
+++ FinalProjectF_2018/Project_Main.c
@@ -1,9 +1,16 @@
+/****************************************************************************
+  Title:       EGR 226 Lab 7
+  Filename:    Lab_7_Lab_Part_
+  Author(s):   Mathew Yerian-French
+  Date:        11/11/2018
+  Instructor:  Professor Scott Zuidema
+  Description:
+  Note:
+*****************************************************************************/
 #include "msp.h"
+#include "LCD.h"
+#include "serial.h"
 
-
-/**
- * main.c
- */
 void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
